1ac6ca1ffef0f49498f710a48093d908f8ef6fa0
pudyasta <pudyastasatria@gmail.com>
2022-11-17 22:53:21 +0700

    fix issue

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ dotenv.config();
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: process.env.LOCAL_DB_HOST,
-    user: process.env.LOCAL_DB_USER,
-    password: process.env.LOCAL_DB_PASSWORD,
-    database: process.env.LOCAL_DB_NAME,
-    port: process.env.LOCAL_DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
   },
   listPerPage: 10,
 };
