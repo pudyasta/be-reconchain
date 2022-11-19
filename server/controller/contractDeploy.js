94c7c97f592c5768294cdbf51128b9d15a2a8af0
pudyasta <pudyastasatria@gmail.com>
2022-11-19 19:13:13 +0700

    handling error

--- server/controller/contractDeploy.js
+++ server/controller/contractDeploy.js
@@ -25,27 +25,31 @@ exports.updateProduct = async function (req, res, next) {
       const row = await db.query(
         `SELECT * FROM products WHERE product_id='${id}'`
       );
-      const data = row[0];
-      const address = await deployProduct(
-        JSON.stringify({ destination, carbon, date, note })
-      );
+      if (row.length > 0) {
+        const data = row[0];
+        const address = await deployProduct(
+          JSON.stringify({ destination, carbon, date, note })
+        );
 
-      const query = db.query(
-        `INSERT INTO products(product_id,product_name,material,chain,product_status,company_code,shipping_status,to_address,longitude,latitude) VALUE('${data.product_id}','${data.product_name}','${data.material}','${address}','${product_status}','${res.data.company_code}','${shipping_status}','${data.chain}','${res.data.longitude}','${res.data.latitude}')`
-      );
-      if (query) {
-        return res.status(200).json({
-          address: address,
-          data: {
-            id,
-            destination,
-            carbon,
-            date,
-            shipping_status,
-            product_status,
-            to_address: data.chain,
-          },
-        });
+        const query = db.query(
+          `INSERT INTO products(product_id,product_name,material,chain,product_status,company_code,shipping_status,to_address,longitude,latitude) VALUE('${data.product_id}','${data.product_name}','${data.material}','${address}','${product_status}','${res.data.company_code}','${shipping_status}','${data.chain}','${res.data.longitude}','${res.data.latitude}')`
+        );
+        if (query) {
+          return res.status(200).json({
+            address: address,
+            data: {
+              id,
+              destination,
+              carbon,
+              date,
+              shipping_status,
+              product_status,
+              to_address: data.chain,
+            },
+          });
+        }
+      } else {
+        return res.status(404).json({ data: "Not found" });
       }
     } catch (error) {
       return res.status(500).json({
